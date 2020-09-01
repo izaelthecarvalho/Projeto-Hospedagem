9e5e7ebda14c233e40827efc169b2385df8e647e
Izael Carvalho <izael.thecarvalho13@gmail.com>
2020-09-01 18:01:05 -0300

    Projeto hospedagem

---
+++ Course/Program.cs
@@ -0,0 +1,46 @@
+﻿using Course.Entities;
+using Course.Entities.Exceptions;
+using System;
+public class ProcessFile
+{
+    public static void Main()
+    {
+        try
+        {
+            Console.Write("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation");
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("check-out date (dd/MM/yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            reservation.UpdateDates(checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+        }
+        catch(DomainException e)
+        {
+            Console.WriteLine("Error in reservation: " + e.Message);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Format error: " + e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unexpected error: " + e.Message);
+        }
+
+    }
+}
